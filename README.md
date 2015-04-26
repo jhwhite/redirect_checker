9ef72ff001855841464d657bf846416c4187fcb7
James White <jody@jody-white.com>
2015-04-26 19:43:27 -0400

    Used requests library. Also updated readme.

--- README.md
+++ README.md
@@ -1,24 +1,29 @@
 #redirect_checker
 
-
 Python tool that checks status codes for a site
 
 At this time it also requires an external file with a list of directories or files to check the status of.
 
+The input file should be in the format of: 
+>/path
+
+For example. If you want to check www.example.com/about and www.example.contact the text file should look like:
+
+```
+/about
+/contact
+```
+
 To run:
 -
 From the command line run:
->python redirect _ checker.py inputfile.txt outputfile.txt
-
-The txt file should be in the format of: 
->/path
-
-Example.
-If you want to check www.example.com
+>python redirect_checker.py inputfile.txt outputfile.txt
 
 The script now prompts the user asking what domain they want to check.
 
 For example:
 >What is the domain you want to check?
 
->example.com
+>http://example.com
+
+**Be sure to include `http://` in front of the url.**
--- redirect_checker.py
+++ redirect_checker.py
@@ -1,23 +1,9 @@
-import httplib
+import requests
+
 from sys import argv
 # -*- coding: utf-8 -*-
 
 script, filename, output = argv
-
-#This function comes from http://stackoverflow.com/questions/1140661/python-get-http-response-code-from-a-url
-#author is Evan Fosmark
-def get_status_code(host, path="/"):
-    """ This function retreives the status code of a website by requesting
-        HEAD data from the host. This means that it only requests the headers.
-        If the host cannot be reached or something else goes wrong, it returns
-        None instead.
-    """
-    try:
-        conn = httplib.HTTPConnection(host)
-        conn.request("HEAD", path)
-        return conn.getresponse().status
-    except StandardError:
-        return None
 
 target = open(output, 'w')
 
@@ -28,18 +14,13 @@ non_301 = ""
 with open(filename) as f:
     for line in f:
         line = line.replace("\n", "")
-        #print line
         path = base_url+line
-        #print path
-        #print type(path)
-        status_code = get_status_code(base_url, line)
+        response = requests.get(path)
+        status_code = response.status_code
         if status_code != 301 :
             non_301 = non_301 + str(status_code) + ' ' + path + '\n'
             i += 1
-        #print str(status_code) + ' ' + path
-        target.write(str(status_code) + ' ' + path + '\n')
 
-#print str(i) + " non-301 redirects found."
 target.write(str(i) + " non-301 redirects found.\n")
 target.write(non_301)
 target.close()
